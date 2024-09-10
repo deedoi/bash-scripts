4509bef6ba509503e792f61a7609bbfaab56fab9
Chaipon <deedoi@gmail.com>
2024-09-10 09:20:26 +0700

    Update password_generator.sh
    using -hex 32 rather than -based64 48 of openssl 3.3.1

--- password-generator/password_generator.sh
+++ password-generator/password_generator.sh
@@ -1,47 +1,33 @@
 #!/bin/bash
 
-echo "This is an improved password generator"
+echo "This is a simple password generator"
+echo "Please enter the length of the password (min 8 chars)"
+read PASS_LENGTH
 
-while true; do
-    echo "Please enter the length of the password (minimum 8 characters)"
-    read PASS_LENGTH
+LOWER='abcdefghijklmnopqrstuvwxyz'
+UPPER='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+NUMBERS='0123456789'
+SPECIAL_CHARS='!@#$%^&*()_+-=[]{}|;:,.<>?'
+ALL_CHARS="${LOWER}${UPPER}${NUMBERS}${SPECIAL_CHARS}"
 
-    if [[ "$PASS_LENGTH" =~ ^[0-9]+$ ]] && [ "$PASS_LENGTH" -ge 8 ]; then
-        break
-    else 
-        echo "Invalid input. Please enter a number 8 or greater"
-        echo "Do you want to try again? (y/n)"
-        read answer
-        if [[ "$answer" != "y" ]]; then
-            echo "Exiting script"
-            exit 1
-        fi
-     fi
-done
-
-#Ensure minimum length of 8 condition
-if ! [[ "$PASS_LENGTH" =~ ^[0-9]+$ ]] || [ "$PASS_LENGTH" -lt 8 ]; then
-    PASS_LENGTH=8
-    echo "Invalid input. Minimum length set to 8 characters"
-fi
-
-# Function to generate password at least two special characters
-gen_passwd() {
-    local pass_length=$1
+for i in $(seq 1 5); do
     # Generate base password
-    base_pass=$(openssl rand -base64 48 | tr -dc 'a-zA-Z0-9' | cut -c1-$((pass_length - 2)))
-
-    # Generate two random special characters
-    special_chars=$(echo '!@#$%^&*()_+-=[]{}|;:,.<>?' | fold -w1 | shuf | head -n2 | tr -d '\n')
-
-    # Combine and shuffle
-    password=$(echo "${base_pass}${special_chars}" | fold -w1 | shuf | tr -d '\n')
-
-    echo "$password"
-}
-
-# Generate 5 passwords
-for p in $(seq 1 5); do
-    gen_passwd "$PASS_LENGTH"
+    PASSWORD=$(openssl rand -hex 32 | tr -dc "${ALL_CHARS}" | head -c $((PASS_LENGTH - 3)))
+    
+    # Add one uppercase letter
+    PASSWORD="${PASSWORD}${UPPER:$((RANDOM % ${#UPPER})):1}"
+    
+    # Add two random special characters
+    PASSWORD="${PASSWORD}${SPECIAL_CHARS:$((RANDOM % ${#SPECIAL_CHARS})):1}"
+    PASSWORD="${PASSWORD}${SPECIAL_CHARS:$((RANDOM % ${#SPECIAL_CHARS})):1}"
+    
+    # Shuffle the password
+    SHUFFLED_PASSWORD=""
+    while [ ${#PASSWORD} -gt 0 ]; do
+        n=$((RANDOM % ${#PASSWORD}))
+        SHUFFLED_PASSWORD="${SHUFFLED_PASSWORD}${PASSWORD:$n:1}"
+        PASSWORD="${PASSWORD:0:$n}${PASSWORD:$((n+1))}"
+    done
+    
+    echo $SHUFFLED_PASSWORD
 done
-
